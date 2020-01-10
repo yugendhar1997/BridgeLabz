dffb5198f816f85d2e9747f8f54704af14793046
yugendhar1997 <58684163+yugendhar1997@users.noreply.github.com>
2020-01-10 09:40:31 +0530

    Update TicTacToe.cs

--- Funtional Programs/TicTacToe.cs
+++ Funtional Programs/TicTacToe.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TicTacToe.cs" company="Bridgelabz">
+// Copyright © 2019  Company="BridgeLabz"
+// </copyright>
+// <creator name="Yugendhar Pyata"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace FunctionalPrograms
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+	/// <summary>
+	/// To TicTacToe Play Game
+	/// </summary>
 	class TicTacToe
 	{
 
-
-
+		/// <summary>
+		/// Plays this instance.
+		/// </summary>
+		public static void Play()
+		{
+			Utility.TicTac();
+		}
 	}
-
 }
