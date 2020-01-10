62fcd6ea992951aa1a7bd4b8023fad79b7d20d43
yugendhar1997 <58684163+yugendhar1997@users.noreply.github.com>
2020-01-10 10:14:48 +0530

    Update Program.cs

--- Algorithm Programs/Program.cs
+++ Algorithm Programs/Program.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Program.cs" company="Bridgelabz">
 // Copyright © 2019  Company="BridgeLabz"
 // </copyright>
@@ -19,81 +19,92 @@ namespace Algorithm_Programs
         /// <param name="args">The arguments.</param>
        public static void Main(string[] args)
         {
-            //// To Perform Test Cases atleast once but in this case created infinite loop
-            while (true) 
-            { 
-                Console.WriteLine();
-                Console.WriteLine("Press 1  : Vending Machine");
-                Console.WriteLine("Press 2  : Day Of Week");
-                Console.WriteLine("Press 3  : Temperature Convertion");
-                Console.WriteLine("Press 4  : Monthly Payment");
-                Console.WriteLine("Press 5  : Sqrt Newtons Method");
-                Console.WriteLine("Press 6  : Permutations Of a String");
-                Console.WriteLine("Press 7  : Binary Search The Word From Word List");
-                Console.WriteLine("Press 8  : Insertion Sort");
-                Console.WriteLine("Press 9  : Bubble Sort");
-                Console.WriteLine("Press 10 : Merge Sort");
-                Console.WriteLine("Press 11 : An Anagram Detection");
-                Console.WriteLine("Press 12 : Prime Numbers b/w Range");
-                Console.WriteLine("Press 13 : Guessing The Number");
-                Console.WriteLine("Press 14 : Decimal Convertion ");
-
-                int press = Convert.ToInt32(Console.ReadLine());
-                switch (press)
+            try
+            {
+                //// To Perform Test Cases atleast once but in this case created infinite loop
+                while (true)
                 {
-                    case 1:
-                        VendingMachine.CalculateNotes();
-                        break;
+                    Console.WriteLine();
+                    Console.WriteLine("Press 1  : Vending Machine");
+                    Console.WriteLine("Press 2  : Day Of Week");
+                    Console.WriteLine("Press 3  : Temperature Convertion");
+                    Console.WriteLine("Press 4  : Monthly Payment");
+                    Console.WriteLine("Press 5  : Sqrt Newtons Method");
+                    Console.WriteLine("Press 6  : Permutations Of a String");
+                    Console.WriteLine("Press 7  : Binary Search The Word From Word List");
+                    Console.WriteLine("Press 8  : Insertion Sort");
+                    Console.WriteLine("Press 9  : Bubble Sort");
+                    Console.WriteLine("Press 10 : Merge Sort");
+                    Console.WriteLine("Press 11 : An Anagram Detection");
+                    Console.WriteLine("Press 12 : Prime Numbers b/w Range");
+                    Console.WriteLine("Press 13 : Guessing The Number");
+                    Console.WriteLine("Press 14 : Decimal Convertion ");
 
-                    case 2:
-                        DayOfWeek.Days();
-                        break;
+                    int press = Convert.ToInt32(Console.ReadLine());
+                    switch (press)
+                    {
+                        case 1:
+                            VendingMachine.CalculateNotes();
+                            break;
 
-                    case 3:
-                        TemperatureConvertion.Conversion();
-                        break;
+                        case 2:
+                            DayOfWeek.Days();
+                            break;
 
-                    case 4:
-                        MonthlyPayment.CalculatePayment();
-                        break;
+                        case 3:
+                            TemperatureConvertion.Conversion();
+                            break;
 
-                    case 5:
-                        SqrtNewtonMethod.NewtonsMethod();
-                        break;
+                        case 4:
+                            MonthlyPayment.CalculatePayment();
+                            break;
 
-                    case 7:
-                        Binary_Search_The_Word.Search();
-                        break;
+                        case 5:
+                            SqrtNewtonMethod.NewtonsMethod();
+                            break;
 
-                    case 8:
-                        Insertion_Sort.Sort();
-                        break;
+                        case 6:
+                            StringPermutation.Permutation();
+                            break;
 
-                    case 9:
-                        Bubble_Sort.Sorting();
-                        break;
+                        case 7:
+                            Binary_Search_The_Word.Search();
+                            break;
 
-                    case 11:
-                        Anagram.DetectAnagram();
-                        break;
+                        case 8:
+                            Insertion_Sort.Sort();
+                            break;
 
-                    case 12:
-                        Prime_Numbers.CheckPrime();
-                        break;
+                        case 9:
+                            Bubble_Sort.Sorting();
+                            break;
 
-                    case 13:
-                        FindNumber.Find();
-                        break;
+                        case 11:
+                            Anagram.DetectAnagram();
+                            break;
 
-                    case 14:
-                        DecimalToBinary.DecimalConversion();
-                        break;
+                        case 12:
+                            Prime_Numbers.CheckPrime();
+                            break;
 
-                    default:
-                        Console.WriteLine("Invalid Choice");
-                        break;
+                        case 13:
+                            FindNumber.Find();
+                            break;
+
+                        case 14:
+                            DecimalToBinary.DecimalConversion();
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid Choice");
+                            break;
+                    }
                 }
-            } 
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
