93f95f31de3805af61daec82bf236243c4f700e4
yugendhar1997 <58684163+yugendhar1997@users.noreply.github.com>
2020-01-10 09:43:14 +0530

    Update Two_Dimensional_Array.cs

--- Funtional Programs/Two_Dimensional_Array.cs
+++ Funtional Programs/Two_Dimensional_Array.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Two_Dimensional_Array.cs" company="Bridgelabz">
+// Copyright © 2019  Company="BridgeLabz"
+// </copyright>
+// <creator name="Yugendhar Pyata"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace FunctionalPrograms
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
     class Two_Dimensional_Array
     {
+        /// <summary>
+        /// this is the method of Array2d class
+        /// </summary>
+        public static void Array()
+        {
+            Utility.Array2d();
+        }
     }
 }
