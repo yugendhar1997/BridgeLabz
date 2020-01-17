8b94326fdbf8c87a8792448a8ba97c518fd3426c
yugendhar1997 <yugendhar1695@gmail.com>
2020-01-17 19:24:57 +0530

    [Yugendhar] Refractor. Main Flow Of Execution Modified

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -19,13 +19,28 @@ namespace OOPS
             while (true)
             {
                 Console.WriteLine("Press 1 : Address Book");
+                Console.WriteLine("Press 2 : Inventory Data");
+                Console.WriteLine("Press 3 : Inventory Management");
+                Console.WriteLine("Press 4 : Stock Account Management");
+                Console.WriteLine("Press 5 : Deck of Cards");
+                Console.WriteLine("Press 6 : Clinique Management");
 
                 int input = Convert.ToInt32(Console.ReadLine());
                 switch (input)
                 {
                     case 1:
-                        AddressBook.AddressBookOperations addressBookOperations = new AddressBook.AddressBookOperations();
+                        AddressBookOperations addressBookOperations = new AddressBookOperations();
                         addressBookOperations.AddressOperation();
+                        break;
+                    case 2:
+                        InventoryOperation inventoryOperation = new InventoryOperation();
+                        inventoryOperation.InventoryManage();
+                        break;
+                    case 3:
+                        InventoryManagement.InventoryDataManagementOperation.DataOperation();
+                        break;
+                    case 4:
+                        DeckOfCards.PlayGame();
                         break;
                 }
             }
