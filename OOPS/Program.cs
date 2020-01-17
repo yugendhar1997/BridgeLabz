6f4e797f541e23eb6260ce846c1a972e89f93620
yugendhar1997 <yugendhar1695@gmail.com>
2020-01-17 12:11:09 +0530

    [Yugendhar] Refractor. Modified Folw Of Execution of Program

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -6,7 +6,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace OOPS
 {
-    using OOPS.Address_Book;
+    using OOPS.AddressBook;
     using System;
 
     /// <summary>
@@ -18,57 +18,16 @@ namespace OOPS
         {
             while (true)
             {
-                
-                Console.WriteLine("\n Perform Any Operation On Inventory Management");
-                Console.WriteLine(" Press 1 : Add New Item");
-                Console.WriteLine(" Press 2 : Update Items");
-                Console.WriteLine(" Press 3 : Remove The Items");
-                Console.WriteLine(" Press 4 : Get Inventory Details");
-
-                int option = Convert.ToInt32(Console.ReadLine());
-
-                switch (option)
-                {
-                    case 1:
-                        InventoryManagement.AddItem();
-                        break;
-                    case 2:
-                        InventoryManagement.Update();
-                        break;
-                    case 3:
-                        InventoryManagement.DeleteItem();
-                        break;
-                    case 4:
-                        InventoryManagement.GetInventoryDetails();
-                        break;
-                }
-                
-                /*
-                Console.WriteLine(" Press 1 : To Add a Person to Address Book");
-                Console.WriteLine(" Press 2 : To Remove a Person From Address Book");
-                Console.WriteLine(" Press 3 : To Search a Person Using First Name in Address Book");
-                Console.WriteLine(" Press 4 : To Print The Total Persons Data from Address Book");
+                Console.WriteLine("Press 1 : Address Book");
 
                 int input = Convert.ToInt32(Console.ReadLine());
-
                 switch (input)
                 {
                     case 1:
-                        AddressBook_Operations.AddPerson();
-                        break;
-                    case 2:
-                        AddressBook_Operations.RemovePerson();
-                        break;
-                    case 3:
-                        AddressBook_Operations.SearchPerson();
-                        break;
-                    case 4:
-                        AddressBook_Operations.ListPeople();
+                        AddressBook.AddressBookOperations addressBookOperations = new AddressBook.AddressBookOperations();
+                        addressBookOperations.AddressOperation();
                         break;
                 }
-                
-
-                //DeckOfCards.Cards(); */
             }
         }
     }
