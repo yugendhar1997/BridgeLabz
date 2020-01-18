c582c2cb8a9e599b524d1373ded30f40734974d6
yugendhar1997 <yugendhar1695@gmail.com>
2020-01-18 11:44:14 +0530

    [Yugendhar] Refractor. Flow of Execution of Total OOPS Programs Logic has Modified

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -6,24 +6,28 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace OOPS
 {
-    using OOPS.AddressBook;
     using System;
+    using OOPS.AddressBook;
+    using OOPS.InventoryManagement;
 
     /// <summary>
     /// Run Total Application
     /// </summary>
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
             while (true)
             {
-                Console.WriteLine("Press 1 : Address Book");
-                Console.WriteLine("Press 2 : Inventory Data");
-                Console.WriteLine("Press 3 : Inventory Management");
-                Console.WriteLine("Press 4 : Stock Account Management");
-                Console.WriteLine("Press 5 : Deck of Cards");
-                Console.WriteLine("Press 6 : Clinique Management");
+                Console.WriteLine(" Press 1 : Address Book");
+                Console.WriteLine(" Press 2 : Inventory Data");
+                Console.WriteLine(" Press 3 : Inventory Management");
+                Console.WriteLine(" Press 4 : Stock Account Management");
+                Console.WriteLine(" Press 5 : Deck of Cards");
 
                 int input = Convert.ToInt32(Console.ReadLine());
                 switch (input)
@@ -40,6 +44,9 @@ namespace OOPS
                         InventoryManagement.InventoryDataManagementOperation.DataOperation();
                         break;
                     case 4:
+
+                        break;
+                    case 5:
                         DeckOfCards.PlayGame();
                         break;
                 }
