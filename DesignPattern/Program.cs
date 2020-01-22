fe0638369239a6efd5b9217298622df331466aa7
yugendhar1997 <yugendhar1695@gmail.com>
2020-01-22 18:57:03 +0530

    [Yugendhar] Refractor. Flow of Execution of Design Patterns Modified

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,12 +1,78 @@
-﻿using System;
-
-namespace DesignPatterns
+﻿namespace DesignPattern
 {
+    using DesignPatterns.BehavioralDesignPatterns;
+    using DesignPatterns.CreationalPattern;
+    using DesignPatterns.Singleton;
+    using DesignPatterns.StructuralDesignPattern;
+    using System;
+    using System.Collections.Generic;
+    using static DesignPatterns.BehavioralDesignPatterns.ChatMediatorImpl;
+    using static DesignPatterns.BehavioralDesignPatterns.MyTopic;
+
+    /// <summary>
+    /// This is main class of Program.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                bool flag;
+                int choice;
+                do
+                {
+                    do
+                    {
+                        Console.WriteLine("");
+                        Console.Write("Enter Your Choice: ");
+                        flag = int.TryParse(Console.ReadLine(), out choice);
+                        if (!flag)
+                        {
+                            Console.WriteLine("Enter input integer");
+                        }
+                    } while (!flag);
+                    flag = false;
+                    switch (choice)
+                    {
+                        case 1:
+                            UtilitySingleton.SingletonType();
+                            break;
+                        case 2:
+                            FactoryClient.ClientRequirement();
+                            break;
+                        case 3:
+                            Cloning.CloningOperation();
+                            break;
+                        case 4:
+                            Utility.VendorAdapterProgram();
+                            break;
+                        case 5:
+                            ProxyDesignPattern.AtmWithdrawal();
+                            break;
+                        case 6:
+                            Utility.ObserverProgram();
+                            break;
+                        case 7:
+                            Utility.ChatMediatorProgram();
+                            break;
+                        case 8:
+                            Utility.Shopping();
+                            break;
+
+                            Console.ReadKey();
+
+                    }
+                } while (!flag);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
