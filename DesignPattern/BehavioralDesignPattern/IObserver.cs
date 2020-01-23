09d2fbbb741ea32af3bfcab0a35a159e3d7d18eb
yugendhar1997 <yugendhar1695@gmail.com>
2020-01-23 19:02:10 +0530

    [Yugendhar] Refractor. Funtion Logic Modified

---
+++ DesignPattern/BehavioralDesignPattern/IObserver.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IObserver.cs" company="Bridgelabz">
+// Copyright © 2019  Company="BridgeLabz"
+// </copyright>
+// <creator name="Yugendhar Pyata"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.BehavioralDesignPattern
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Observer Interface
+    /// </summary>
+    public interface IObserver
+    {
+        /// <summary>
+        /// Updates this instance.
+        /// </summary>
+        public void Update();
+
+        /// <summary>
+        /// Sets the subject.
+        /// </summary>
+        /// <param name="sub">The sub.</param>
+        public void SetSubject(ISubject sub);
+    }
+
+    /// <summary>
+    /// Subject Interface
+    /// </summary>
+    public interface ISubject
+    {
+        /// <summary>
+        /// Registers the specified object.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        public void Register(IObserver obj);
+
+        /// <summary>
+        /// Unregisters the specified object.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        public void Unregister(IObserver obj);
+
+        /// <summary>
+        /// Notifies the observers.
+        /// </summary>
+        public void NotifyObservers();
+
+        /// <summary>
+        /// Gets the update.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns>obj</returns>
+       public Object GetUpdate(IObserver obj);
+
+        /// <summary>
+        /// Gets the update.
+        /// </summary>
+        /// <returns></returns>
+        string GetUpdate();
+    }
+}
