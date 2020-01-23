575f2e7da01c26a1526572e69690670609b3af37
yugendhar1997 <yugendhar1695@gmail.com>
2020-01-23 19:03:21 +0530

    [Yugendhar] Refractor. Abstract Methods Modified

---
+++ DesignPattern/BehavioralDesignPattern/IShoppingCartVisitor.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ShoppingCartVisitor.cs" company="Bridgelabz">
+// Copyright © 2019  Company="BridgeLabz"
+// </copyright>
+// <creator name="Yugendhar Pyata"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.BehavioralDesignPattern
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This is an interface for ShoppingCartVisitor.
+    /// </summary>
+    public interface IShoppingCartVisitor
+    {
+        /// <summary>
+        /// Visits the specified book.
+        /// </summary>
+        /// <param name="book">The book.</param>
+        /// <returns> book </returns>
+        int Visit(Book book);
+
+        /// <summary>
+        /// Visits the specified fruit.
+        /// </summary>
+        /// <param name="fruit">The fruit.</param>
+        /// <returns> fruit </returns>
+        int Visit(Fruit fruit);
+    }
+
+    /// <summary>
+    /// this is an interface of ItemElement.
+    /// </summary>
+    public interface ItemElement
+    {
+        /// <summary>
+        /// Accepts the specified visitor.
+        /// </summary>
+        /// <param name="visitor">The visitor.</param>
+        /// <returns> visitor </returns>
+        public int Accept(IShoppingCartVisitor visitor);
+    }
+}
