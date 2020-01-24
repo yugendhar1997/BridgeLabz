9658a628a18ea4c19dee39a4f75cf80cab829042
yugendhar1997 <yugendhar1695@gmail.com>
2020-01-24 19:15:16 +0530

    [Yugendhar] Refractor. Details Of Address Book Properties Modified

---
+++ DesignPattern/StructuralDesignPattern/IAddressBookDetails.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IAddressBookDetails.cs" company="Bridgelabz">
+// Copyright © 2019  Company="BridgeLabz"
+// </copyright>
+// <creator name="Yugendhar Pyata"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.StructuralDesignPattern
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This is the interface for Address Book Details.
+    /// </summary>
+    public interface IAddressBookDetails
+    {
+        /// <summary>
+        /// method for Name instance.
+        /// </summary>
+        /// <returns>The Name</returns>
+        public string Name();
+
+        /// <summary>
+        /// Address instance.
+        /// </summary>
+        /// <returns>The Address</returns>
+        public string Address();
+
+        /// <summary>
+        /// State instance.
+        /// </summary>
+        /// <returns>The State</returns>
+        public string State();
+
+        /// <summary>
+        /// City instance.
+        /// </summary>
+        /// <returns>The City</returns>
+        public string City();
+
+        /// <summary>
+        /// Phone no instance.
+        /// </summary>
+        /// <returns>The Phone Number</returns>
+        public int PhoneNo();
+
+        /// <summary>
+        /// Zip code instance.
+        /// </summary>
+        /// <returns>The Zip code</returns>
+        public int ZipCode();
+    }
+}
