60843dafc1784379378d0a30595bf0f22d386c01
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-12 17:31:17 +0530

    [Yugendhar] Refactor. Main Function Modified

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,30 +1,34 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file=Program.cs" company="Bridgelabz">
-//   Copyright © 2019 Company="BridgeLabz"
+// <copyright file="Program.cs" company="Bridgelabz">
+// Copyright © 2019  Company="BridgeLabz"
 // </copyright>
-// <creator name="Yugendhar"/>
+// <creator name="Yugendhar Pyata"/>
 // --------------------------------------------------------------------------------------------------------------------
-
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
 namespace EmployeeManagement
 {
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    /// <summary>
+    /// Total Application Run
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        /// <summary>
+        /// Creates the web host builder.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+/        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
