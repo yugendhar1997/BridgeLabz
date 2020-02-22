6727f9c7a6f0ab946fd975c191130e9d935e5b70
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-22 18:42:19 +0530

    [Yugendhar] Add. Manager Project Added

---
+++ FundooBackEnd/Manager/Interface/IAccount.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IAccount.cs" company="Bridgelabz">
+// Copyright © 2020  Company="BridgeLabz"
+// </copyright>
+// <creator name="Yugendhar Pyata"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Manager.Interface
+{
+    using Model;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Account Interface
+    /// </summary>
+    public interface IAccount
+    {
+        /// <summary>
+        /// Logins the asynchronous.
+        /// </summary>
+        /// <param name="loginModel">The login model.</param>
+        /// <returns></returns>
+        Task<string> LoginAsync(LoginModel loginModel);
+
+        /// <summary>
+        /// Registrations the asynchronous.
+        /// </summary>
+        /// <param name="userModel">The user model.</param>
+        /// <returns></returns>
+        Task<bool> RegistrationAsync(UserModels userModel);
+
+        /// <summary>
+        /// Forgets the password.
+        /// </summary>
+        /// <param name="resetModel">The reset model.</param>
+        /// <returns></returns>
+        Task<bool> ForgetPassword(ForgetPasswordModel resetModel);
+
+        /// <summary>
+        /// Resets the password.
+        /// </summary>
+        /// <param name="resetModel">The reset model.</param>
+        /// <returns></returns>
+        Task<bool> ResetPassword(ResetPasswordModel resetModel);
+
+        /// <summary>
+        /// Faces the book login asynchronous.
+        /// </summary>
+        /// <param name="loginModel">The login model.</param>
+        /// <returns></returns>
+        Task<string> FaceBookLoginAsync(LoginModel loginModel);
+    }
+}
