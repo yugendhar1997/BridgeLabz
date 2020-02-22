b81f06abcf2f57fec532a57296f38a1efac9a973
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-22 18:40:57 +0530

    [Yugendhar] Refactor. Changed Program class

--- FundooBackEnd/Fundoo/Startup.cs
+++ FundooBackEnd/Fundoo/Startup.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Startup.cs" company="Bridgelabz">
+// Copyright © 2020  Company="BridgeLabz"
+// </copyright>
+// <creator name="Yugendhar Pyata"/>
+// --------------------------------------------------------------------------------------------------------------------
+using Manager.Interface;
+using Manager.Manager;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +15,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Model;
 using Repository;
+using Repository.Interface;
+using Repository.Repository;
 
 namespace Fundoo
 {
@@ -32,6 +42,8 @@ namespace Fundoo
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<IAccount, AccountManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
