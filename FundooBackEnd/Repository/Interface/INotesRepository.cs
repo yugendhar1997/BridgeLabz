d8802fcd3e6366bc8946bfc5e6fd4c8061d44d73
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-26 18:54:03 +0530

    [Yugendhar] Refactor. Notes Repository Changed

---
+++ FundooBackEnd/Repository/Interface/INotesRepository.cs
@@ -0,0 +1,48 @@
+﻿using Model.NotesModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    /// <summary>
+    /// Notes Repository
+    /// </summary>
+    public interface INotesRepository
+    {
+        /// <summary>
+        /// Adds the notes.
+        /// </summary>
+        /// <param name="notesModel">The notes model.</param>
+        /// <returns></returns>
+        Task AddNotes(NotesModel notesModel);
+
+        /// <summary>
+        /// Deletes the notes.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Task DeleteNotes(int id);
+
+        /// <summary>
+        /// Updates the notes.
+        /// </summary>
+        /// <param name="notesModel">The notes model.</param>
+        /// <returns></returns>
+        Task UpdateNotes(NotesModel notesModel);
+
+        /// <summary>
+        /// Gets the list.
+        /// </summary>
+        /// <returns></returns>
+        List<NotesModel> GetList();
+
+        /// <summary>
+        /// Gets the notes.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        List<NotesModel> GetNotes(int id);
+    }
+}
