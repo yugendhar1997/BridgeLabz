86399b0c2da7ec4c1ca3587da69a53b65322e9a6
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-26 19:01:55 +0530

    [Yugendhar] Refactor. Notes Manager Implemented

---
+++ FundooBackEnd/Manager/Manager/NotesManager.cs
@@ -0,0 +1,135 @@
+﻿using Manager.Interface;
+using Model.NotesModel;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Manager
+{
+    /// <summary>
+    /// Notes Manager class
+    /// </summary>
+    /// <seealso cref="Manager.Interface.INotes" />
+    public class NotesManager : INotes
+    {
+        /// <summary>
+        /// The notes repository
+        /// </summary>
+        private readonly INotesRepository notesRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotesManager"/> class.
+        /// </summary>
+        /// <param name="notesRepository">The notes repository.</param>
+        public NotesManager(INotesRepository notesRepository)
+        {
+            this.notesRepository = notesRepository;
+        }
+
+        /// <summary>
+        /// Adds the asynchronous.
+        /// </summary>
+        /// <param name="notesModel">The notes model.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<string> AddAsync(NotesModel notesModel)
+        {
+            try
+            {
+                await this.notesRepository.AddNotes(notesModel);
+                return "NOTES ADDED SUCCESSFULLY";
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the asynchronous.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<string> DeleteAsync(int id)
+        {
+            try
+            {
+                await this.notesRepository.DeleteNotes(id);
+                return "NOTES DELETED SUCESSFULLY";
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Updates the asynchronous.
+        /// </summary>
+        /// <param name="notesModel">The notes model.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<string> UpdateAsync(NotesModel notesModel)
+        {
+            try
+            {
+                await this.notesRepository.UpdateNotes(notesModel);
+                return "NOTES DETAILS UPDATED SUCCESSFULLY";
+            }
+            catch(Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// Gets all list asynchronous.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public List<NotesModel> GetAllListAsync()
+        {
+            try
+            {
+                var list = new List<NotesModel>();
+                var result = this.notesRepository.GetList();
+                foreach(var item in result)
+                {
+                    list.Add(item);
+                }
+                return list;
+            }
+            catch(Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// Gets the notes asynchronous.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public List<NotesModel> GetNotesAsync(int id)
+        {
+            try
+            {
+                var list = new List<NotesModel>();
+                var result = this.notesRepository.GetNotes(id);
+                foreach (var item in result)
+                {
+                    list.Add(item);
+                }
+                return list;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
