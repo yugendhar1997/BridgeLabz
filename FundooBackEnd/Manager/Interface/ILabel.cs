0fef030356398c5df8c3e492a024109b825284f3
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-26 19:02:51 +0530

    [Yugendhar] Refactor. Label Interface Changed

---
+++ FundooBackEnd/Manager/Interface/ILabel.cs
@@ -0,0 +1,47 @@
+﻿using Model.LabelModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Interface
+{
+    /// <summary>
+    /// Label Manager interface
+    /// </summary>
+    public interface ILabel
+    {
+        /// <summary>
+        /// Adds the specified label model.
+        /// </summary>
+        /// <param name="labelModel">The label model.</param>
+        /// <returns></returns>
+        string Add(LabelModel labelModel);
+
+        /// <summary>
+        /// Updates the specified label model.
+        /// </summary>
+        /// <param name="labelModel">The label model.</param>
+        /// <returns></returns>
+        string Update(LabelModel labelModel);
+
+        /// <summary>
+        /// Deletes the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        string Delete(int id);
+
+        /// <summary>
+        /// Gets all list.
+        /// </summary>
+        /// <returns></returns>
+        List<LabelModel> GetAllList();
+
+        /// <summary>
+        /// Gets the label.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        List<LabelModel> GetLabel(int id);
+    }
+}
