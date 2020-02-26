8e3315f08e735653e049c97448816c9f69be22e1
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-26 16:39:45 +0530

    [Yugendhar] Refactor. Configuration Services Added

--- FundooBackEnd/Fundoo/Startup.cs
+++ FundooBackEnd/Fundoo/Startup.cs
@@ -17,6 +17,7 @@ using Model;
 using Repository;
 using Repository.Interface;
 using Repository.Repository;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Fundoo
 {
@@ -40,24 +41,53 @@ namespace Fundoo
             var appSettingsSection = this.Configuration.GetSection("AppSettings");
             services.Configure<AppSetting>(appSettingsSection);
 
+            services.AddMvc();
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
+            //// Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info 
+                {
+                    Title = "FUNDOO NOTES API",
+                    Version = "v1" ,
+                    Description="ASP.NET core WEB API"
+                });
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddTransient<IAccountRepository, AccountRepository>();
             services.AddTransient<IAccount, AccountManager>();
+            services.AddTransient<INotesRepository, NotesRepository>();
+            services.AddTransient<INotes, NotesManager>();
+            services.AddTransient<ILabel, LabelManager>();
+            services.AddTransient<ILabelRepository, LabelRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            app.UseCors("MyPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FUNDOO NOTE API");
+                });
             }
             else
             {
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseMvc();
         }
