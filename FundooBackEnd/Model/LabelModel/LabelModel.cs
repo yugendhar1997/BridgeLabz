78bf78205678aa68a54d22677962af217a3c70f5
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-26 18:57:08 +0530

    [Yugendhar] Refactor. Label Models Changed

---
+++ FundooBackEnd/Model/LabelModel/LabelModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Model.LabelModel
+{
+    /// <summary>
+    /// Label Model
+    /// </summary>
+    public class LabelModel
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("UserModels")]
+        public string Email { get; set; }
+        public string Label { get; set; }
+    }
+}
