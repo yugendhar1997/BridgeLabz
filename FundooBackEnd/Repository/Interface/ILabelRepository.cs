7ac6a956838f88caf6f93a3d95ed76f1232ed362
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-26 18:55:08 +0530

    [Yugendhar] Refactor. Label Repository Modified

---
+++ FundooBackEnd/Repository/Interface/ILabelRepository.cs
@@ -0,0 +1,47 @@
+﻿using Model.LabelModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Interface
+{
+    /// <summary>
+    /// Label Repository
+    /// </summary>
+    public interface ILabelRepository
+    {
+        /// <summary>
+        /// Adds the label.
+        /// </summary>
+        /// <param name="labelModel">The label model.</param>
+        /// <returns></returns>
+        string AddLabel(LabelModel labelModel);
+
+        /// <summary>
+        /// Updates the label.
+        /// </summary>
+        /// <param name="labelModel">The label model.</param>
+        /// <returns></returns>
+        string UpdateLabel(LabelModel labelModel);
+
+        /// <summary>
+        /// Deletes the label.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        string DeleteLabel(int id);
+
+        /// <summary>
+        /// Gets the list.
+        /// </summary>
+        /// <returns></returns>
+        List<LabelModel> GetList();
+
+        /// <summary>
+        /// Gets the label.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        List<LabelModel> GetLabel(int id);
+    }
+}
