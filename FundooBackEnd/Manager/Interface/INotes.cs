f345cc5664b906c63795672984bac3f5aaf12a04
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-29 15:25:43 +0530

    [Yugendhar] Notes Interface changed

--- FundooBackEnd/Manager/Interface/INotes.cs
+++ FundooBackEnd/Manager/Interface/INotes.cs
@@ -1,11 +1,18 @@
-﻿using Model.NotesModel;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="INotes.cs" company="Bridgelabz">
+// Copyright © 2020  Company="BridgeLabz"
+// </copyright>
+// <creator name="Yugendhar Pyata"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace Manager.Interface
 {
+    using Microsoft.AspNetCore.Http;
+    using Model.NotesModel;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
     /// <summary>
     /// Notes Manager Interface
     /// </summary>
@@ -44,5 +51,95 @@ namespace Manager.Interface
         /// <param name="id">The identifier.</param>
         /// <returns></returns>
         List<NotesModel> GetNotesAsync(int id);
+
+        /// <summary>
+        /// Determines whether [is archive asynchronous] [the specified identifier].
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Task<string> IsArchiveAsync(int id);
+
+        /// <summary>
+        /// Uns the archive asynchronous.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Task<string> UnArchiveAsync(int id);
+
+        /// <summary>
+        /// Determines whether [is trash asynchronous] [the specified identifier].
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Task<string> IsTrashAsync(int id);
+
+        /// <summary>
+        /// Trashes the list.
+        /// </summary>
+        /// <returns></returns>
+        List<NotesModel> TrashList();
+
+        /// <summary>
+        /// Restores the asynchronous.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Task<string> RestoreAsync(int id);
+
+        /// <summary>
+        /// Determines whether [is pin asynchronous] [the specified identifier].
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Task<string> IsPinAsync(int id);
+
+        /// <summary>
+        /// Uns the pin asynchronous.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Task<string> UnPinAsync(int id);
+
+        /// <summary>
+        /// Uploads the images.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="image">The image.</param>
+        /// <returns></returns>
+        string UploadImages(int id, IFormFile image);
+
+        /// <summary>
+        /// Reminders the value.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="reminder">The reminder.</param>
+        /// <returns></returns>
+        string ReminderValue(int id, string reminder);
+
+        /// <summary>
+        /// Restores all note.
+        /// </summary>
+        /// <returns></returns>
+        Task<string> RestoreAllNote();
+
+        /// <summary>
+        /// Removes all trash.
+        /// </summary>
+        /// <returns></returns>
+        Task<string> RemoveAllTrash();
+
+        /// <summary>
+        /// Sets the color.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="color">The color.</param>
+        /// <returns></returns>
+        Task<string> SetColor(int id, string color);
+
+        /// <summary>
+        /// Gets the archive list.
+        /// </summary>
+        /// <returns></returns>
+        List<NotesModel> GetArchiveList();
     }
 }
