3c3945aa069004d45fb33f5d35dcfc98cf4c7581
yugendhar1997 <yugendhar1695@gmail.com>
2020-02-29 15:31:08 +0530

    [Yugendhar] Label Model Modified

--- FundooBackEnd/Model/LabelModel/LabelModel.cs
+++ FundooBackEnd/Model/LabelModel/LabelModel.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LabelModel.cs" company="Bridgelabz">
+// Copyright © 2020  Company="BridgeLabz"
+// </copyright>
+// <creator name="Yugendhar Pyata"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace Model.LabelModel
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Text;
+
     /// <summary>
     /// Label Model
     /// </summary>
