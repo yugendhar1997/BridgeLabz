23d8de0a216f949147ccd232f1e201d24309412c
yugendhar1997 <yugendhar1695@gmail.com>
2020-03-06 12:19:44 +0530

    [Yugendahr] Refactor Interface Collabarator Implemented

---
+++ FundooBackEnd/Manager/Interface/ICollaborator.cs
@@ -0,0 +1,28 @@
+﻿using Model.Collaborator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Interface
+{
+    /// <summary>
+    /// Collaborator Interface
+    /// </summary>
+    public interface ICollaborator
+    {
+        /// <summary>
+        /// Adds the collaborator asynchronous.
+        /// </summary>
+        /// <param name="collaboratorsModel">The collaborators model.</param>
+        /// <returns></returns>
+        Task<string> AddCollaboratorAsync(CollaboratorsModel collaboratorsModel);
+
+        /// <summary>
+        /// Removes the collaborator asynchronous.
+        /// </summary>
+        /// <param name="collaboratorsModel">The collaborators model.</param>
+        /// <returns></returns>
+        Task<string> RemoveCollaboratorAsync(CollaboratorsModel collaboratorsModel);
+    }
+}
