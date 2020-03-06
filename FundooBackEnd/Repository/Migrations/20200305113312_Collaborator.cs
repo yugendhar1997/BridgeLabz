6d8048b2569fd690a9a6893ad019ddec1d20409a
yugendhar1997 <yugendhar1695@gmail.com>
2020-03-06 12:24:29 +0530

    [Yugendhar] Refactor Collabarator Migrations Updated

---
+++ FundooBackEnd/Repository/Migrations/20200305113312_Collaborator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class Collaborator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Collaborators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SenderEmail = table.Column<string>(nullable: true),
+                    NoteId = table.Column<int>(nullable: false),
+                    ReciverEmail = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collaborators", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Collaborators");
+        }
+    }
+}
