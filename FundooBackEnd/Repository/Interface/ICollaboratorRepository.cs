20d4ea9b54bb8f38388e5144823da8e8639414a3
yugendhar1997 <yugendhar1695@gmail.com>
2020-03-06 12:14:50 +0530

    [Yugendahr] Refactor Interface Collabarator Repository Implemented

---
+++ FundooBackEnd/Repository/Interface/ICollaboratorRepository.cs
@@ -0,0 +1,28 @@
+﻿using Model.Collaborator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    /// <summary>
+    /// Collaborator Repository Interface
+    /// </summary>
+    public interface ICollaboratorRepository
+    {
+        /// <summary>
+        /// Adds the collaborators to notes.
+        /// </summary>
+        /// <param name="collaboratorsModel">The collaborators model.</param>
+        /// <returns></returns>
+        Task AddCollaboratorsToNotes(CollaboratorsModel collaboratorsModel);
+
+        /// <summary>
+        /// Deletes the collaborators notes.
+        /// </summary>
+        /// <param name="collaboratorsModel">The collaborators model.</param>
+        /// <returns></returns>
+        Task DeleteCollaboratorsNotes(CollaboratorsModel collaboratorsModel);
+    }
+}
