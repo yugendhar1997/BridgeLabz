140a5e3237fbd503262cf5f1a444988e58d4b5bd
yugendhar1997 <yugendhar1695@gmail.com>
2020-03-06 12:13:43 +0530

    [Yugendahr] Refactor Admin repository interface Implemented

---
+++ FundooBackEnd/Repository/Interface/IAdminRepository.cs
@@ -0,0 +1,15 @@
+﻿using Model.AdminModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    public interface IAdminRepository
+    {
+        Task AddAdmin(AdminLoginModel adminLogin);
+        Task<string> AdminLogin(AdminLoginModel adminLogin);
+        List<AdminModel> GetList();
+    }
+}
