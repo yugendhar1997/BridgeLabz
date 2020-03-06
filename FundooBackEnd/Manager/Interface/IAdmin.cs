84c5f7740c82a6338b09fdedaedb0631194ee948
yugendhar1997 <yugendhar1695@gmail.com>
2020-03-06 12:18:01 +0530

    [Yugendahr] Refactor Admin Interface Implemented

---
+++ FundooBackEnd/Manager/Interface/IAdmin.cs
@@ -0,0 +1,16 @@
+﻿using Model.AdminModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Interface
+{
+    public interface IAdmin
+    {
+        Task AddAdminAsync(AdminLoginModel adminLogin);
+        Task<string> AdminLoginAsync(AdminLoginModel adminLogin);
+
+        List<AdminModel> GetAllListAsync();
+    }
+}
