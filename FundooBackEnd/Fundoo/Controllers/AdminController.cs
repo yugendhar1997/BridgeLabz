fa1437cb5a0e88b19141b5d30d17005092fdfbf9
yugendhar1997 <yugendhar1695@gmail.com>
2020-03-06 12:16:11 +0530

    [Yugendahr] Refactor Admin Controller Implemented

---
+++ FundooBackEnd/Fundoo/Controllers/AdminController.cs
@@ -0,0 +1,59 @@
+﻿using Manager.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Model.AdminModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fundoo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdmin admin;
+
+        public AdminController(IAdmin admin)
+        {
+            this.admin = admin;
+        }
+
+        [HttpPost]
+        [Route("adding")]
+        public IActionResult AddAdminAsync(AdminLoginModel adminLogin)
+        {
+            try
+            {
+                var result = this.admin.AddAdminAsync(adminLogin);
+                return this.Ok(new { result });
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LoginAsync(AdminLoginModel adminLogin)
+        {
+            try
+            {
+                var result = await this.admin.AdminLoginAsync(adminLogin);
+                return this.Ok(new { result });
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("loginlist")]
+        public List<AdminModel> List()
+        {
+            return this.admin.GetAllListAsync();
+        }
+    }
+}
